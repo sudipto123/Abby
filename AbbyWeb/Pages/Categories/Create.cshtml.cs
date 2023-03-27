af9e1a4af36434c4eaa7b64facb87494ab56ca2e
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-27 20:55:38 +0530

    62. Create Category UI

---
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,20 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
