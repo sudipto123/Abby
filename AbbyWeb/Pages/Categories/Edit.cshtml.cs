09a55f7512e21dd5692c6a963afdb49da936375b
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-27 22:00:30 +0530

    Edit and Delete Category

---
+++ AbbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,38 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Category.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(Category);
+                _db.SaveChanges();
+                //TempData["success"] = "Category updated successfully";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
