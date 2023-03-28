37fba32104c579a56cffffd8f884c15ec685d7d5
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-28 12:05:34 +0530

    Error Toastr implemented

--- AbbyWeb/Pages/Categories/Delete.cshtml.cs
+++ AbbyWeb/Pages/Categories/Delete.cshtml.cs
@@ -32,7 +32,7 @@ namespace AbbyWeb.Pages.Categories
             }
             _db.Category.Remove(obj);
             await _db.SaveChangesAsync();
-            TempData["success"] = "Category deleted successfully";
+            TempData["error"] = "Category deleted successfully";
             return RedirectToPage("Index");
         }
     }
